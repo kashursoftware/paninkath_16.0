29bc98562d8a0f09a68182a4c07d9efe6801b564
Amjad <paninkath@gmail.com>
2016-08-21 20:04:14 +0530

    modularized code

---
+++ paninkath/www/js/com/Reg.js
@@ -0,0 +1,16 @@
+function registerUser(){
+
+	
+	$.ajax({
+		url: "http://vps.hilfe.website:3800/addUser",
+		type: "get", //send it through get method
+		data:{"fName":$("#fName").val(),"lName": $("#lName").val() , "email":$("#uEmail").val(),"userName":$("#nUName").val(),"passWord":$("#nUPwd").val(),"cPassWord":$("#nUCPwd").val()},
+		success: function(response) {
+			console.log("sent......");
+		},
+		error: function(xhr) {
+			console.log(xhr);
+		}
+	});
+		
+};
