39150e8fec721b04a81a9d95c254065c558524d7
Amjad <paninkath@gmail.com>
2016-08-25 23:49:22 +0530

    code update

--- paninkath/platforms/android/assets/www/js/util/validationRules.js
+++ paninkath/platforms/android/assets/www/js/util/validationRules.js
@@ -1,31 +1,74 @@
 function validateSignUp1(){
 	
+	jQuery.validator.addMethod("lettersonly", function(value, element) {
+		return this.optional(element) || /^[a-z]+$/i.test(value);
+	}, "Letters only please"); 
+
 	$("#signUpForm1").validate({
+		
+		onfocusout: function (element) { $(element).valid();},
 		rules: {
-			fName: "required",
-			lName: "required",
+			fName: {
+				
+				required: true,
+				lettersonly: true
+			},
+			lName: {
+				
+				required: true,
+				lettersonly: true
+			},
 			uEmail: {
 				required: true,
 				email: true
 			}
 		},
 		messages: {
-			fName: "Please enter your firstname",
-			lName: "Please enter your lastname",
-			uEmail: "Please enter a valid email address"
-		}
-	
+			fName: "Valid first name please...",
+			lName: "Valid first name please...",
+			uEmail: "Valid email please..."
+		},errorPlacement: function (error, element) {
+			
+			
+            var lastError = $(element).data('lastError'),
+                newError = $(error).text();
+				
+			//$("#errorMsg1").text(newError);
+
+            $(element).data('lastError', newError);
+			
+
+            if(newError !== '' && newError !== lastError){
+                $(element).tooltipster('content', newError);
+                $(element).tooltipster('show');
+            }
+			
+			//$(element).attr("placeholder",newError);
+			$(element).addClass("invalid-value");
+			
+			
+        },
+        success: function (label, element) {
+            $(element).tooltipster('hide');
+			$(element).removeClass("invalid-value");
+        }
 	});
 	
+	
 };
+
+
 
 //##############################################################################################
 //##############################################################################################
 // validate signup form on keyup and submit
 
 function validateSignUp2(){
+	
 		
 	$("#signUpForm2").validate({
+		
+		onfocusout: function (element) { $(element).valid();},
 		rules: {
 			nUName: {
 				required: true,
@@ -55,6 +98,25 @@ function validateSignUp2(){
 				minlength: "Your password must be at least 5 characters long",
 				equalTo: "Please enter the same password as above"
 			}
-		}
+		},errorPlacement: function (error, element) {
+			
+			
+           // var lastError = $(element).data('lastError'),
+           var newError = $(error).text();
+		   
+		   $("#errorMsg2").text(newError);
+				
+
+            $(element).data('lastError', newError);
+			
+			$(element).attr("placeholder",newError);
+			$(element).addClass("invalid-value");
+			
+			
+        },
+        success: function (label, element) {
+            $(element).tooltipster('hide');
+			$(element).removeClass("invalid-value");
+        }
 	});
 };
--- paninkath/www/js/util/validationRules.js
+++ paninkath/www/js/util/validationRules.js
@@ -1,46 +1,59 @@
 function validateSignUp1(){
 	
-	
+	jQuery.validator.addMethod("lettersonly", function(value, element) {
+		return this.optional(element) || /^[a-z]+$/i.test(value);
+	}, "Letters only please"); 
+
 	$("#signUpForm1").validate({
 		
 		onfocusout: function (element) { $(element).valid();},
 		rules: {
-			fName: "required",
-			lName: "required",
+			fName: {
+				
+				required: true,
+				lettersonly: true
+			},
+			lName: {
+				
+				required: true,
+				lettersonly: true
+			},
 			uEmail: {
 				required: true,
 				email: true
 			}
 		},
 		messages: {
-			fName: "Please enter your firstname",
-			lName: "Please enter your lastname",
-			uEmail: "Please enter a valid email address"
+			fName: "Valid first name please...",
+			lName: "Valid first name please...",
+			uEmail: "Valid email please..."
 		},errorPlacement: function (error, element) {
+			
+			
             var lastError = $(element).data('lastError'),
                 newError = $(error).text();
+				
+			//$("#errorMsg1").text(newError);
 
             $(element).data('lastError', newError);
+			
 
             if(newError !== '' && newError !== lastError){
                 $(element).tooltipster('content', newError);
                 $(element).tooltipster('show');
             }
+			
+			//$(element).attr("placeholder",newError);
+			$(element).addClass("invalid-value");
+			
+			
         },
         success: function (label, element) {
             $(element).tooltipster('hide');
+			$(element).removeClass("invalid-value");
         }
 	});
 	
-	
-	// propose username by combining first- and lastname
-		/*$("#fName").focus(function() {
-			var firstname = $("#fName").val();
-			var lastname = $("#lName").val();
-			if (firstname && lastname && !this.value) {
-				this.value = firstname + "." + lastname;
-			}
-		});*/
 	
 };
 
@@ -51,8 +64,11 @@ function validateSignUp1(){
 // validate signup form on keyup and submit
 
 function validateSignUp2(){
+	
 		
 	$("#signUpForm2").validate({
+		
+		onfocusout: function (element) { $(element).valid();},
 		rules: {
 			nUName: {
 				required: true,
@@ -82,6 +98,25 @@ function validateSignUp2(){
 				minlength: "Your password must be at least 5 characters long",
 				equalTo: "Please enter the same password as above"
 			}
-		}
+		},errorPlacement: function (error, element) {
+			
+			
+           // var lastError = $(element).data('lastError'),
+           var newError = $(error).text();
+		   
+		   $("#errorMsg2").text(newError);
+				
+
+            $(element).data('lastError', newError);
+			
+			$(element).attr("placeholder",newError);
+			$(element).addClass("invalid-value");
+			
+			
+        },
+        success: function (label, element) {
+            $(element).tooltipster('hide');
+			$(element).removeClass("invalid-value");
+        }
 	});
 };
